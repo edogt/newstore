e40bb6a5ff8ae0bc269054f14bdff9d534371bc9
Eduardo Gutiérrez <edogt@hotmail.com>
2024-03-06 22:08:16 -0300

    my first commit

---
+++ app/app2.ts
@@ -0,0 +1,7 @@
+// Definimos una función que imprime "Hello, world!" en la consola
+function sayHello() {
+    console.log("Hello, world!");
+  }
+  
+  // Llamamos a la función para que imprima el mensaje
+  sayHello();
